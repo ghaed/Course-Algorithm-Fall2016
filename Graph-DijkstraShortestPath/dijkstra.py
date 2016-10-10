9b1396fa15e134f1d4d2026d61de0ba67c4937a1
Hassan Ghaed <mhaghaed@gmail.com>
2016-10-10 02:21:36 -0400

    Heap-based Dijkstra's algorithm working

--- Graph-DijkstraShortestPath/dijkstra.py
+++ Graph-DijkstraShortestPath/dijkstra.py
@@ -1,4 +1,5 @@
 """ Dijkstra's shortest path algorithm"""
+import heapq    # Heap class, we will use tuples to maintain key-vertice mapping
 
 
 class Node:
@@ -28,13 +29,13 @@ class Graph(object):
         """ Constructor to initialize an empty graph. Must add nodes later """
         self.nodes = {}         # Each node number will be its hash
 
-    def add_node(self, node_id, node_obj=None):
+    def add_node(self, node_id_new, node_obj=None):
         """ Adds a new isolated node to graph.
         optionally, it can be given a pre-allocated node object"""
         if not node_obj:
-            self.nodes[node_id] = Node()
+            self.nodes[node_id_new] = Node()
         else:
-            self.nodes[node_id] = node_obj
+            self.nodes[node_id_new] = node_obj
 
     def read_graph_from_file(self, file_name):
         """ Reads a graph from a file. Each row starts with a node name followed by all of its target nodes.
@@ -44,34 +45,34 @@ class Graph(object):
         for line in lines:
             line = line.rstrip()
             line_strings = line.split()
-            node_id = int(line_strings[0])
+            node_id_new = int(line_strings[0])
 
-            self.add_node(node_id)
+            self.add_node(node_id_new)
             for edge_string in line_strings[1:]:
                 head = int(edge_string.split(',')[0])
                 length = int(edge_string.split(',')[1])
-                self.nodes[node_id].add_edge(head, length)
+                self.nodes[node_id_new].add_edge(head, length)
 
     def print_graph(self):
         """ Prints a graph"""
-        for node_id in self.nodes.keys():
-            print node_id, ':', self.nodes[node_id].edges, ' , dist=', self.nodes[node_id].distance
+        for node_id_new in self.nodes.keys():
+            print node_id_new, ':', self.nodes[node_id_new].edges, ' , dist=', self.nodes[node_id_new].distance
 
     def is_edge(self, node_id_a, node_id_b):
         """ Checks if there is an edge between two node names"""
         if node_id_a not in self.nodes.keys() or node_id_b not in self.nodes.keys():
             return False
-        edges_a = self.nodes[node_id_a].edges
+        edges_a = self.nodes[node_id_a].edges.keys()
         if node_id_b in edges_a:
             return True
         else:
             return False
 
-    def remove(self, node_id):
+    def remove(self, node_id_new):
         """ Deletes a certain node ID"""
-        self.nodes.pop(node_id, None)
+        self.nodes.pop(node_id_new, None)
         for n in self.nodes.keys():
-            self.nodes[n].disconnect_from_node(node_id)
+            self.nodes[n].disconnect_from_node(node_id_new)
 
     def add_edge(self, node_id_a, node_id_b, length):
         """ Adds an edge between two nodes"""
@@ -80,9 +81,9 @@ class Graph(object):
     def get_edges(self):
         """Return a list of tuples corresponding to the edges"""
         all_edges = []
-        for node_id, node in self.nodes.iteritems():
+        for node_id_new, node in self.nodes.iteritems():
             for edge in node.edges:
-                all_edges.append((node_id, edge))
+                all_edges.append((node_id_new, edge))
         return all_edges
 
     def get_edge_count(self):
@@ -107,9 +108,9 @@ class PathGraph(Graph):
     def clone(self):
         """ Clones a graph object"""
         cut_graph = PathGraph()
-        for node_id in self.nodes.keys():
-            node = self.nodes[node_id].clone()
-            cut_graph.add_node(node_id, node)
+        for node_id_new in self.nodes.keys():
+            node = self.nodes[node_id_new].clone()
+            cut_graph.add_node(node_id_new, node)
         return cut_graph
 
     def dijkstra_naive(self, node_id_start):
@@ -124,20 +125,50 @@ class PathGraph(Graph):
                         cur_distance = self.nodes[tail].distance + cur_len
                         if cur_distance < best_distance:
                             best_head = head
-                            best_tail = tail
                             best_distance = cur_distance
             self.x.append(best_head)
             self.nodes[best_head].distance = best_distance
+
+    def dijkstra_heap(self, node_id_start):
+        self.nodes[node_id_start].distance = 0
+        self.x.append(node_id_start)        # The list of explored vertices (all bot x_bar)
+        x_bar = []      # Our heap
+        for nid in self.nodes.keys():  # Initialize heap
+            if nid == node_id_start:
+                continue
+            distance = 1e9
+            if self.is_edge(node_id_start, nid):
+                distance = self.nodes[node_id_start].edges[nid]
+            heapq.heappush(x_bar, (distance, nid))
+
+        for _ in range(self.node_count - 1):        # Sweep through the vertices
+            (best_distance, best_head) = heapq.heappop(x_bar)
+            self.nodes[best_head].distance = best_distance
+            self.x.append(best_head)
+
+            for head, length in self.nodes[best_head].edges.iteritems():    # Update the frontier if need be
+                if head in self.x:   # Focus on the arches inside the V-X, the arches back to X don't matter
+                    continue
+                score_new = best_distance + length
+                # Find the old score.This defeats the purpose, but, let's keep if for now
+                for (score, nid) in x_bar:
+                    if nid == head:
+                        score_old = score
+                if score_new < score_old:
+                    x_bar.remove((score_old, head))
+                    heapq.heapify(x_bar)
+                    heapq.heappush(x_bar, (score_new, head))
+
 
 g = PathGraph()
 test_case = 'test_case_large.txt'
 g.read_graph_from_file(test_case)
 g.print_graph()
-g.dijkstra_naive(1)
+g.dijkstra_heap(1)      # Can also do the _naive version
 print 'After Dijkstra'
 g.print_graph()
-if 'large' in test_case:
+if 'large' in test_case:    # The correct answer is 2599,2610,2947,2052,2367,2399,2029,2442,2505,3068
     result = ""
-    for node_id in [7,37,59,82,99,115,133,165,188,197]:
+    for node_id in [7, 37, 59, 82, 99, 115, 133, 165, 188, 197]:
         result += str(g.nodes[node_id].distance) + ','
     print 'result: ', result
