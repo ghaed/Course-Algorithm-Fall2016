db6d445a139bd5d24955cb28f2ddcd0f515137ad
ghaed <Sadaf@Saeedehs-MacBook-Air.local>
2016-11-30 21:19:31 +0000

    Made the recursive Knapsack algorithm smarter by re-using already-solved cases

--- DP-Knapsack/KnapsackRecursive.py
+++ DP-Knapsack/KnapsackRecursive.py
@@ -7,6 +7,7 @@ class KnapsackRecursive(object):
         self.capacity = 0   # Total capacity, i.e. max total weight
         self.num_items = 0  # Number of items to consider
         self.solution_value = 0 # Final solution
+        self.solutions = {}     # A hash table of the solutions
 
     def read_from_file(self, file_name):
         """ Reads from a file.
@@ -37,6 +38,7 @@ class KnapsackRecursive(object):
         - num_remaining_items: determines the items to look at, i.e. tells the function to look at
         self.v[0:num_remaining_items]
         """
+        # Top case
         if num_remaining_items == -1 and remaining_capacity == -1:
             num_remaining_items = self.num_items
             remaining_capacity = self.capacity
@@ -44,17 +46,26 @@ class KnapsackRecursive(object):
         else:
             final_solution = False
 
+        # Base case
         if num_remaining_items < 1:
             return 0
+
+        # Cases that are already solved
+        if (num_remaining_items, remaining_capacity) in self.solutions:
+            return self.solutions[(num_remaining_items, remaining_capacity)]
+
+        # Cases to be solved
         option_not_selected = self.solve(num_remaining_items-1, remaining_capacity)
         if self.w[num_remaining_items] > remaining_capacity:
             option_selected = option_not_selected
         else:
             option_selected = self.solve(num_remaining_items-1, remaining_capacity-self.w[num_remaining_items]) +\
                               self.v[num_remaining_items]
-        if final_solution:
-            self.solution_value = max(option_selected, option_not_selected)
-        return max(option_selected, option_not_selected)
+        solution_value = max(option_selected, option_not_selected)
+        self.solutions[(num_remaining_items, remaining_capacity)] = solution_value
+        if final_solution:  # Save the solution in class object
+            self.solution_value = solution_value
+        return solution_value
 
     @property
     def status(self):
@@ -67,13 +78,12 @@ class KnapsackRecursive(object):
         return result
 
 
-
 k = KnapsackRecursive()
-k.read_from_file('test_case_13.txt')
+# k.read_from_file('test_case_13.txt')
 # k.read_from_file('test_case_5513.txt')
-# k.read_from_file('knapsack_big.txt')
-print k.status
+k.read_from_file('knapsack_big.txt')
+# print k.status
 k.solve()
-print k.status
+# print k.status
 print 'solution value =', k.solution_value
 # solution = 2493893
